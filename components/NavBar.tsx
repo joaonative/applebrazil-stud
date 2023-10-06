82b1e12aa559ce6d8550605b634a3c40c3949ff8
joaonative <jestudos127@gmail.com>
2023-10-06 19:03:36 -0300

    add 'NavBarItem' into 'NavBar' component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import NavBarItem from "./NavBarItem";
 
 const NavBar = () => {
   return (
-    <header className="flex flex-row justify-center items-center gap-50 px-60">
+    <header className="flex flex-row justify-center items-center gap-12">
       <img src="/applelogo.svg" alt="apple logo" />
+      <NavBarItem title="Loja" />
+      <NavBarItem title="Mac" />
+      <NavBarItem title="iPad" />
+      <NavBarItem title="iPhone" />
+      <NavBarItem title="Watch" />
+      <NavBarItem title="AirDots" />
+      <NavBarItem title="Tv e casa" />
+      <NavBarItem title="Entreterimento" />
+      <NavBarItem title="Acessorios" />
+      <NavBarItem title="Suporte" />
     </header>
   );
 };
