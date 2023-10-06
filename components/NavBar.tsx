02233920f60c740440ece7ff2783d00727e79767
joaonative <jestudos127@gmail.com>
2023-10-06 18:55:59 -0300

    created 'NavBar' component and add into 'layout'

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header className="flex flex-row justify-center items-center gap-50 px-60">
+      <img src="/applelogo.svg" alt="apple logo" />
+    </header>
+  );
+};
+
+export default NavBar;
