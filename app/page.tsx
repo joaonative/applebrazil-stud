5b1c3b2d196195c25917c00587d49280bc7e2d68
joaonative <jestudos127@gmail.com>
2023-10-06 20:02:44 -0300

    fixed 'Title' component to support different colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Title from "@/components/Title";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>title.</main>;
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="max-w-2xl">
+        <Title
+          text="Loja."
+          secText="O melhor jeito de comprar o que você ama."
+        />
+      </div>
+    </div>
+  );
 }
--- components/Title.tsx
+++ components/Title.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Title = ({ text, secText }: { text: string; secText: string }) => {
   return (
-    <h1 className="text-4xl font-semibold flex flex-row">
-      {text}
-      <p className="text-sectitle">{secText}</p>
-    </h1>
+    <div className="flex text-5xl font-semibold">
+      <p className="">
+        {text}
+        <span className="text-sectitle">{secText}</span>
+      </p>
+    </div>
   );
 };
 
