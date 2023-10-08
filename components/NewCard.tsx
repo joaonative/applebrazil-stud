2ae9f8c7bf3d10bc035ece7f596f48e32b947ff0
joaonative <jestudos127@gmail.com>
2023-10-08 17:19:24 -0300

    created 'NewCard' component

---
+++ components/NewCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const NewCard = ({
+  productName,
+  title,
+  price,
+  bg,
+}: {
+  productName: string;
+  title: string;
+  price: number;
+  bg: number;
+}) => {
+  return (
+    <div
+      className={`flex flex-col justify-start gap-4 px-5 py-10 rounded-2xl w-[400px] h-[500px] bg-cover text-white bg-${bg}`}
+    >
+      <h2 className="text-md uppercase">{productName}</h2>
+      <h1 className="text-3xl font-semibold">{title}</h1>
+      <h2 className="text-lg">A partir de R$ {price}</h2>
+    </div>
+  );
+};
+export default NewCard;
