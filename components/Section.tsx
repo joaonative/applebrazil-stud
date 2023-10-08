44a6ff7836af6440e8ec80c34a383a63268fd7ac
joaonative <jestudos127@gmail.com>
2023-10-08 17:20:00 -0300

    created 'Section' component

---
+++ components/Section.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Title from "./Title";
+
+const Section = ({
+  children,
+  sectionTitle,
+  sectionSecTitle,
+}: {
+  children: React.ReactNode;
+  sectionTitle: string;
+  sectionSecTitle: string;
+}) => {
+  return (
+    <section className="flex flex-col gap-5">
+      <Title text={sectionTitle} secText={sectionSecTitle} />
+      {children}
+    </section>
+  );
+};
+
+export default Section;
