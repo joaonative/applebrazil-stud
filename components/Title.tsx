fa1b4ab7d1ee541676d3763d31fff570c3f60000
joaonative <jestudos127@gmail.com>
2023-10-09 15:59:57 -0300

    new props to 'Title' to create different sizes of 'text'

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-const Title = ({ text, secText }: { text: string; secText: string }) => {
+const Title = ({
+  text,
+  secText,
+  sm,
+}: {
+  text: string;
+  secText: string;
+  sm?: boolean;
+}) => {
   return (
-    <div className="flex text-5xl font-semibold">
-      <p className="">
+    <div className={`flex font-semibold ${sm ? "text-3xl" : "text-5xl"}`}>
+      <p>
         {text}
         <span className="text-sectitle">{secText}</span>
       </p>
